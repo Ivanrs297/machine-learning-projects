8db8c74b442e9ab407c48381d6911f051b0eacb8
Ivan <ivanrs297@gmail.com>
2022-05-30 17:51:26 -0500

    Results of Energy Generation Analysis

--- energy-generation-analysis/utils.py
+++ energy-generation-analysis/utils.py
@@ -23,24 +23,25 @@ def split_sequence(data, n_steps):
 
 def plot_history(history):
 
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 10))
 
-    # summarize history for mean_squared_error
-    plt.subplot(121)
-    plt.plot(history.history['mean_squared_error'])
-    plt.plot(history.history['val_mean_squared_error'])
-    plt.title('model mean_squared_error')
-    plt.ylabel('mean_squared_error')
-    plt.xlabel('epoch')
-    plt.legend(['Train', 'Validation'], loc='upper left')
+    # # summarize history for mean_squared_error
+    # plt.subplot(121)
+    # plt.plot(history.history['mean_squared_error'])
+    # plt.plot(history.history['val_mean_squared_error'])
+    # plt.title('model mean_squared_error')
+    # plt.ylabel('mean_squared_error')
+    # plt.xlabel('epoch')
+    # plt.legend(['Train', 'Validation'], loc='upper left')
 
     # summarize history for loss
-    plt.subplot(122)
+    # plt.subplot(122)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
     plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.savefig('results/Loss Training.png')
 
     plt.show()
